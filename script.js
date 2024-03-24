baa7b791055204f87fbc1d726d364f74244f8fc0
ArfinHasib <arfinhasib21375@gmail.com>
2024-03-25 01:08:16 +0600

    Updated Functionality

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // let apiQuotes = [];
-const quoteContainer = document.getElementById('quote-generator');
+
+const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+   loader.hidden = false;
+   quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+   quoteContainer.hidden = false;
+   loader.hidden = true;
+}
 
 // Show New Quote
 function newQuote() {
+   showLoadingSpinner();
    // Pick a random quote from the apiQuotes array
    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
 
@@ -22,7 +35,9 @@ function newQuote() {
       quoteText.classList.remove('long-quote');
    }
 
+   // Set quote, hide loader
    quoteText.textContent = quote.text;
+   removeLoadingSpinner();
 }
 
 // Tweet Quote

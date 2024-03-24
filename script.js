b364ec92cb26dffd264fadccbc3f08bde5465e82
ArfinHasib <arfinhasib21375@gmail.com>
2024-03-24 17:49:41 +0600

    New quote generate with the button and the functionality to post tweet with the tweet button

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 // let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-generator');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
 // Show New Quote
 function newQuote() {
    // Pick a random quote from the apiQuotes array
    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-   console.log(quote);
+
+   if (!quote.author) {
+      authorText.textContent = 'Unknown';
+   } else {
+      authorText.textContent = quote.author;
+   }
+
+   if (quote.text.length > 50) {
+      quoteText.classList.add('long-quote');
+   } else {
+      quoteText.classList.remove('long-quote');
+   }
+
+   quoteText.textContent = quote.text;
 }
+
+// Tweet Quote
+function tweetQuote() {
+   const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+   window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // Get Quotes form the API
 // async function getQuotes() {
